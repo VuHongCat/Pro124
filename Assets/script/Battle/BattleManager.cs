b8382a6133796df6d0be5dd3f18122ab8136d911
VuHongCat <catbao54@gmail.com>
2026-07-25 00:50:52 +0700

    Card Loop

--- Assets/script/Battle/BattleManager.cs
+++ Assets/script/Battle/BattleManager.cs
@@ -8,6 +8,7 @@ public class BattleManager : MonoBehaviour
     [SerializeField] private CardDatabase database;
     [SerializeField] private HandManager handManager;
     [SerializeField] private DeckManager deckManager;
+    [SerializeField] private EnergyManager energyManager;
 
     private void Start()
     {
@@ -16,5 +17,23 @@ public class BattleManager : MonoBehaviour
         {
             handManager.AddCard(card);
         }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            energyManager.SpendEnergy(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            energyManager.ResetEnergy();
+        }
+    }
+
+    public void PlayCard(CardDisplay card)
+    {
+        deckManager.AddToDiscard(card.CardData);
+        handManager.RemoveCard(card);
     }
 }
