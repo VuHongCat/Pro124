eb287fa1df213442d09e18e29ae7d4ca4fbedc81
VuHongCat <catbao54@gmail.com>
2026-07-27 01:31:00 +0700

    Player block

--- Assets/script/Enemy/EnemyCombat.cs
+++ Assets/script/Enemy/EnemyCombat.cs
@@ -6,6 +6,7 @@ public class EnemyCombat : MonoBehaviour
     [SerializeField] private EnemyData enemyData;
     public event Action OnAttackFinished;
     private EnemyIntent enemyIntent;
+    private int turnCount;
     public void Initialize(EnemyData data)
     {
         enemyData = data;
@@ -17,11 +18,47 @@ public class EnemyCombat : MonoBehaviour
         player.TakeDamage(enemyData.attackDamage);
 
         OnAttackFinished?.Invoke();
-        DecideNextIntent();
     }
 
     public void DecideNextIntent()
     {
-        enemyIntent.SetIntent(EnemyIntentType.Attack, enemyData.attackDamage);
+        turnCount++;
+        if (turnCount % 2 == 1)
+        {
+            enemyIntent.SetIntent(
+                EnemyIntentType.Attack,
+                enemyData.attackDamage
+            );
+        }
+        else
+        {
+            enemyIntent.SetIntent(
+                EnemyIntentType.Block,
+                6
+            );
+        }
+    }
+
+    public void ExecuteIntent(PlayerHealth player)
+    {
+        switch (enemyIntent.IntentType)
+        {
+            case EnemyIntentType.Attack:
+                Attack(player);
+                break;
+
+            case EnemyIntentType.Block:
+                Debug.Log("Enemy Block");
+                break;
+
+            case EnemyIntentType.Buff:
+                Debug.Log("Enemy Buff");
+                break;
+
+            case EnemyIntentType.Debuff:
+                Debug.Log("Enemy Debuff");
+                break;
+        }
+        DecideNextIntent();
     }
 }
