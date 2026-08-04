feff07378070e0762cc72706346c92700d7c7c97
vuanhduy007-cyber <Vuanhduy007@gmail.com>
2026-08-04 13:54:44 +0700

    update worldmap

---
+++ Assets/script/Map/WorldIslandUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldIslandUI : MonoBehaviour
+{
+    public int islandID;
+    public string sceneName;
+
+    private bool unlocked;
+
+
+    public void UpdateIsland()
+    {
+        int unlockedIsland = PlayerPrefs.GetInt("UnlockedIsland", 1);
+
+        unlocked = islandID <= unlockedIsland;
+    }
+
+
+    public void ClickIsland()
+    {
+        if (!unlocked)
+            return;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
